da5fab61c5b180e29e60c866ca9bc92ecae22d2f
JaeHyun Lee <myleje@gmail.com>
2022-03-11 10:38:03 +0900

    PlayerCtrl 스크립트 수정 LJH

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -7,7 +7,8 @@ public class PlayerCtrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test");
+        Debug.Log("Test Test");
+        Debug.Log("충돌발생 예정 LJH");
     }
 
     // Update is called once per frame
